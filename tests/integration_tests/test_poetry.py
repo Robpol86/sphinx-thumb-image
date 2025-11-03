d3a4fbcd87be140a5ab33fc6ae9eea1623a690d6
Robpol86 <robpol86@gmail.com>
2025-11-03 15:18:19 +0900

    fixed lockfile check

--- tests/integration_tests/test_poetry.py
+++ tests/integration_tests/test_poetry.py
@@ -10,9 +10,16 @@ from _pytest.monkeypatch import MonkeyPatch
 def test_lock(monkeypatch: MonkeyPatch):
     """Verify lock file is up to date."""
     monkeypatch.delenv("UV_FROZEN")
-    output = subprocess.check_output(["uv", "lock", "--check"], stderr=subprocess.STDOUT).strip()
-    assert output
-    matches = re.findall(rb"The lockfile .+ needs to be updated", output, re.MULTILINE)
+    process = subprocess.run(["uv", "lock", "--check"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout = process.stdout.decode("utf8")
+    # If up to date:
+    if process.returncode == 0:
+        assert re.findall(r"Resolved \d+ packages? in", stdout)
+        return
+    # If out of date print helpful message
+    matches = re.findall(r"The lockfile .+ needs to be updated", stdout)
     if matches:
         logging.getLogger(__name__).warning(matches[0])
         raise RuntimeError("Update lock file with: make relock")
+    # If another error fail
+    assert not stdout
