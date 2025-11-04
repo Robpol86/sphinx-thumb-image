65cd898e0342c7ed07eb7d2176994c4b8232a707
Robpol86 <robpol86@gmail.com>
2025-11-04 12:49:49 +0900

    start with 0

--- tests/unit_tests/test_docs/test-defaults/index.rst
+++ tests/unit_tests/test_docs/test-defaults/index.rst
@@ -1,15 +1,15 @@
-.. thumb-image:: _images/tux-01.png
+.. thumb-image:: _images/tux-00.png
 
-.. thumb-figure:: _images/tux-02.png
+.. thumb-figure:: _images/tux-01.png
 
-.. thumb-image:: _images/tux-03.png
+.. thumb-image:: _images/tux-02.png
     :target: https://google.com
 
-.. thumb-figure:: _images/tux-04.png
+.. thumb-figure:: _images/tux-03.png
     :target: https://aol.com
 
-.. thumb-image:: _images/tux-05.png
+.. thumb-image:: _images/tux-04.png
     :no-target:
 
-.. thumb-figure:: _images/tux-06.png
+.. thumb-figure:: _images/tux-05.png
     :no-target:
--- tests/unit_tests/test_rendered_defaults.py
+++ tests/unit_tests/test_rendered_defaults.py
@@ -11,7 +11,9 @@ from .utils import assert_image
 @pytest.mark.sphinx("html", testroot="defaults")
 def test_index_rst(img_tags: List[element.Tag]):
     """Test."""
-    assert_image(img_tags[0], "_images/tux-01.png")
-    assert_image(img_tags[1], "_images/tux-02.png")
-    assert_image(img_tags[2], "_images/tux-03.png", "https://google.com")
-    assert_image(img_tags[3], "_images/tux-04.png", "https://aol.com")
+    assert_image(img_tags[0], "_images/tux-00.png")  # TODO _images/tux-00.png
+    assert_image(img_tags[1], "_images/tux-01.png")  # TODO _images/tux-01.png
+    assert_image(img_tags[2], "_images/tux-02.png", "https://google.com")
+    assert_image(img_tags[3], "_images/tux-03.png", "https://aol.com")
+    assert_image(img_tags[4], "_images/tux-04.png")
+    assert_image(img_tags[5], "_images/tux-05.png")
