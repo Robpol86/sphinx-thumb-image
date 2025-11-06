8c354f980b5f775489228eae7e1010200c95acca
Robpol86 <robpol86@gmail.com>
2025-11-06 17:02:41 +0900

    no do_assert

--- tests/unit_tests/test_docs_defaults.py
+++ tests/unit_tests/test_docs_defaults.py
@@ -1,6 +1,6 @@
 """Tests."""
 
-from typing import List, Optional
+from typing import List
 
 import pytest
 from bs4 import element
@@ -22,27 +22,19 @@ def test_target(img_tags: List[element.Tag], request: pytest.FixtureRequest):
     #       https://github.com/User/Repo/blob/%(path)s
     #       https://github.com/User/Repo/blob/docs/images/%(filename)s
     """
-    def do_assert(image: element.Tag, href: Optional[str] = None):
-        """Assert HTML image has the expected link or lack thereof."""
-        target = image.parent
-        if href is None:
-            assert target.name != "a"
-        else:
-            assert target.name == "a"
-            assert target.get("href") == href
-    do_assert(img_tags[0], "https://google.com")
-    do_assert(img_tags[1], "https://aol.com")
+    assert img_tags[0].parent.get("href") == "https://google.com"
+    assert img_tags[1].parent.get("href") == "https://aol.com"
     pytest.skip("TODO")
-    do_assert(img_tags[2], "_images/tux.png")
-    do_assert(img_tags[3], "_images/tux.png")
-    do_assert(img_tags[4], None)
-    do_assert(img_tags[5], None)
-    do_assert(img_tags[6], "https://github.com/User/Repo/blob/_images/tux.png")
-    do_assert(img_tags[7], "https://github.com/User/Repo/blob/docs/images/tux.png")
+    assert img_tags[2].parent.get("href") == "_images/tux.png"
+    assert img_tags[3].parent.get("href") == "_images/tux.png"
+    assert img_tags[4].parent.name != "a"
+    assert img_tags[5].parent.name != "a"
+    assert img_tags[6].parent.get("href") == "https://github.com/User/Repo/blob/_images/tux.png"
+    assert img_tags[7].parent.get("href") == "https://github.com/User/Repo/blob/docs/images/tux.png"
     thumb_image_default_target = request.node.callspec.params["sphinx_app"]["thumb_image_default_target"]
     if thumb_image_default_target in ["__omit__", "original"]:
-        do_assert(img_tags[8], "_images/tux.png")
-        do_assert(img_tags[9], "_images/tux.png")
+        assert img_tags[8].parent.get("href") == "_images/tux.png"
+        assert img_tags[9].parent.get("href") == "_images/tux.png"
     else:
         pytest.skip("TODO Not Implemented")
 
