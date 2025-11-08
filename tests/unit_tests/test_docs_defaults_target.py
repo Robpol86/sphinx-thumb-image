a4f3137e414b631a422b68aa177f6372178d9cd4
Robpol86 <robpol86@gmail.com>
2025-11-08 15:09:34 +0900

    test_sub_target TODO

--- tests/unit_tests/test_docs_defaults_target.py
+++ tests/unit_tests/test_docs_defaults_target.py
@@ -44,7 +44,23 @@ def test_target(request: pytest.FixtureRequest, img_tags: list[element.Tag]):
         assert img_tags[9].parent.name != "a"
     else:
         pytest.fail("Unhandled thumb_image_default_target value")
-    
+
+
+@pytest.mark.sphinx("html", testroot="defaults", confoverrides={"master_doc": "sub/target"})
+def test_sub_target(request: pytest.FixtureRequest, img_tags: list[element.Tag]):
+    """TODO."""
+    pytest.skip("TODO")
+    assert img_tags[0].parent.get("href") == "https://google.com"
+    assert img_tags[1].parent.get("href") == "https://aol.com/?x=%sample"
+    assert img_tags[2].parent.get("href") == "_images/tux.png"
+    assert img_tags[3].parent.get("href") == "_images/tux.png"
+    assert img_tags[4].parent.name != "a"
+    assert img_tags[5].parent.name != "a"
+    assert img_tags[6].parent.get("href") == "https://github.com/User/Repo/blob/_images/tux.png"
+    assert img_tags[7].parent.get("href") == "https://cloudflare.com/cdn/tux.png"
+    thumb_image_default_target = request.node.callspec.params["app_params"]["thumb_image_default_target"]
+    assert thumb_image_default_target
+
 
 def test_img_src():
     """Test.
