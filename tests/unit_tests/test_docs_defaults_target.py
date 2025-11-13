086971dfab311c3cb2f34cb928303d49aab503a9
Robpol86 <robpol86@gmail.com>
2025-11-13 11:11:26 +0900

    todos

--- tests/unit_tests/test_docs_defaults_target.py
+++ tests/unit_tests/test_docs_defaults_target.py
@@ -64,5 +64,7 @@ def test_img_src():
     TODO::
     * If original not linked by any directive then it should not be in the _build dir.
     * If no thumbs are used (low res) then _thumbs shouldn't exist.
+    * REMEMBER: not all images in _images, can be in arbitrary locations.
+    * Test two thumb-image:: using the same image but different resolutions.
     """
     pytest.skip("TODO")
