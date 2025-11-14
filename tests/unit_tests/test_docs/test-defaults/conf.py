9228f74c64424630caf727921b5caed2f31cfc21
Robpol86 <robpol86@gmail.com>
2025-11-14 11:27:16 +0900

    Merge extension.py into __init__.py.

--- tests/unit_tests/test_docs/test-defaults/conf.py
+++ tests/unit_tests/test_docs/test-defaults/conf.py
@@ -1,7 +1,7 @@
 """Sphinx test configuration."""
 
 exclude_patterns = ["_build"]
-extensions = ["sphinx_thumb_image.extension"]
+extensions = ["sphinx_thumb_image"]
 html_theme = "basic"
 # master_doc = "index"  # Set via confoverrides
 nitpicky = True
