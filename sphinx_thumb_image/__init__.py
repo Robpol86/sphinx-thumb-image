cf41854f644af5ddf62a3803d93b5049507f3bf5
Robpol86 <robpol86@gmail.com>
2025-11-14 03:32:39 +0100

    Style: setup() in __init__.py (#15)
    * Split off into extension.py
    
    * rename thumb to directives
    
    * Merge extension.py into __init__.py.

--- sphinx_thumb_image/__init__.py
+++ sphinx_thumb_image/__init__.py
@@ -1,5 +1,38 @@
-"""Sphinx Thumb Image."""
+"""Sphinx Thumb Image.
+
+Sphinx extension that resizes images into thumbnails on the fly.
+
+https://sphinx-thumb-image.readthedocs.io
+https://github.com/Robpol86/sphinx-thumb-image
+https://pypi.org/project/sphinx-thumb-image
+"""
+
+from sphinx.application import Sphinx
+
+from sphinx_thumb_image.directives import ThumbFigure, ThumbImage
 
 __author__ = "@Robpol86"
 __license__ = "BSD-2-Clause"
 __version__ = "0.0.1"
+
+
+def setup(app: Sphinx) -> dict[str, str]:
+    """Register extension components with Sphinx (called by Sphinx during phase 0 [initialization]).
+
+    :param app: Sphinx application object.
+
+    :returns: Extension version.
+    """
+    app.add_config_value("thumb_image_default_ext", "jpg", "html")
+    app.add_config_value("thumb_image_default_format", None, "html")
+    app.add_config_value("thumb_image_default_height", None, "html")
+    app.add_config_value("thumb_image_default_quality", 100, "html")
+    app.add_config_value("thumb_image_default_target", "fullsize", "html")
+    app.add_config_value("thumb_image_default_width", None, "html")
+    app.add_directive("thumb-image", ThumbImage)
+    app.add_directive("thumb-figure", ThumbFigure)
+    return {
+        "parallel_read_safe": False,  # TODO
+        "parallel_write_safe": False,  # TODO
+        "version": __version__,
+    }
--- sphinx_thumb_image/directives.py
+++ sphinx_thumb_image/directives.py
@@ -1,8 +1,4 @@
-"""Sphinx extension that resizes images into thumbnails on the fly.
-
-https://sphinx-thumb-image.readthedocs.io
-https://github.com/Robpol86/sphinx-thumb-image
-https://pypi.org/project/sphinx-thumb-image
+"""TODO.
 
 TODO::
 * Support pdf and non-html builders
@@ -30,9 +26,7 @@ from pathlib import Path
 from docutils.nodes import Element
 from docutils.parsers.rst import directives
 from docutils.parsers.rst.directives.images import Figure, Image
-from sphinx.application import Sphinx
 
-from sphinx_thumb_image import __version__
 from sphinx_thumb_image.utils import format_target
 
 
@@ -103,25 +97,3 @@ class ThumbFigure(Figure, ThumbCommon):
         """Entrypoint."""
         self._ThumbCommon__update_target()
         return super().run()
-
-
-def setup(app: Sphinx) -> dict[str, str]:
-    """Register extension components with Sphinx (called by Sphinx during phase 0 [initialization]).
-
-    :param app: Sphinx application object.
-
-    :returns: Extension version.
-    """
-    app.add_config_value("thumb_image_default_ext", "jpg", "html")
-    app.add_config_value("thumb_image_default_format", None, "html")
-    app.add_config_value("thumb_image_default_height", None, "html")
-    app.add_config_value("thumb_image_default_quality", 100, "html")
-    app.add_config_value("thumb_image_default_target", "fullsize", "html")
-    app.add_config_value("thumb_image_default_width", None, "html")
-    app.add_directive("thumb-image", ThumbImage)
-    app.add_directive("thumb-figure", ThumbFigure)
-    return {
-        "parallel_read_safe": False,  # TODO
-        "parallel_write_safe": False,  # TODO
-        "version": __version__,
-    }
