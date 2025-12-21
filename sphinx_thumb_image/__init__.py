42fee6a440a647d40568de3ca1392fad0ca8775a
Robpol86 <robpol86@gmail.com>
2025-12-21 14:27:27 -0800

    keeps the .doctrees path in img src

--- sphinx_thumb_image/__init__.py
+++ sphinx_thumb_image/__init__.py
@@ -8,11 +8,14 @@ https://pypi.org/project/sphinx-thumb-image
 """
 
 import os
+import pdb
 
+import PIL.Image
 from sphinx.application import Sphinx
 from sphinx.transforms.post_transforms import SphinxPostTransform
 
 from sphinx_thumb_image.directives import ThumbFigure, ThumbImage
+from sphinx_thumb_image.lib import ThumbRequest
 
 __author__ = "@Robpol86"
 __license__ = "BSD-2-Clause"
@@ -24,8 +27,6 @@ def todo_write_started(app, builder):
     # TODO Builder.post_process_images() is what determines final image file names
     # TODO node["candidates"]?
     if os.environ.get("RP_PDB", ""):
-        import pdb
-
         pdb.set_trace()
 
 
@@ -36,10 +37,18 @@ class TodoPostTransform(SphinxPostTransform):
 
     def run(self, **kwargs):
         """TODO."""
-        if os.environ.get("RP_PDB", ""):
-            import pdb
-
-            pdb.set_trace()
+        for node in self.document.findall(lambda n: ThumbRequest.KEY in n):
+            request: ThumbRequest = node[ThumbRequest.KEY]
+            source = self.env.srcdir / node["candidates"].pop("*")
+            target = self.env.doctreedir / "_thumbs" / f"{source.stem}.th{source.suffix}"  # TODO collisions
+            target.parent.mkdir(exist_ok=True)
+            with PIL.Image.open(source) as image:
+                image.thumbnail((request.width or image.size[0], request.height or image.size[1]))
+                image.save(target)
+                mimetype = image.get_format_mimetype()
+            node["candidates"][mimetype] = node["uri"] = str(target)
+            if os.environ.get("RP_PDB", ""):
+                pdb.set_trace()
 
 
 def setup(app: Sphinx) -> dict[str, str]:
--- tests/unit_tests/__init__.py
+++ tests/unit_tests/__init__.py
@@ -3,4 +3,5 @@
 TODO::
 - Test filenotfound, same error as ..image
 - Test http image linked, ..image would work but we need to fail if non-local
+- Test file with no extension?
 """
