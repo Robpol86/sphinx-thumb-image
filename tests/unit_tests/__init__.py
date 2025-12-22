c72827acdd2bc7c36d21c489c7c893aba314ca6b
Robpol86 <robpol86@gmail.com>
2025-12-21 16:38:12 -0800

    todos

--- tests/unit_tests/__init__.py
+++ tests/unit_tests/__init__.py
@@ -3,5 +3,7 @@
 TODO::
 - Test filenotfound, same error as ..image
 - Test http image linked, ..image would work but we need to fail if non-local
+- Test data: image
+- Test dir/image.* (asset.py)
 - Test file with no extension?
 """
