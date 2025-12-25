8551a9a50acee9df04d1ad4bbcc7054c0e972cc8
Robpol86 <robpol86@gmail.com>
2025-12-25 10:55:07 -0600

    s/scale/resize/ (#18)
    * s/scale/resize/
    
    Style change. Renaming the directive option/Sphinx config name.
    
    * don't update todo

--- sphinx_thumb_image/directives.py
+++ sphinx_thumb_image/directives.py
@@ -30,19 +30,19 @@ class ThumbCommon(Image):
     """Common methods for both thumb image/figure subclassed directives."""
 
     __option_spec = {}
-    __option_spec["scale-width"] = lambda arg: directives.nonnegative_int(arg.replace("px", ""))
-    __option_spec["scale-height"] = __option_spec["scale-width"]
+    __option_spec["resize-width"] = lambda arg: directives.nonnegative_int(arg.replace("px", ""))
+    __option_spec["resize-height"] = __option_spec["resize-width"]
 
-    def __get_scale_size(self):
+    def __get_resize_size(self):
         """TODO."""
         config = self.state.document.settings.env.config
-        thumb_image_scale_width = config["thumb_image_scale_width"]
-        thumb_image_scale_height = config["thumb_image_scale_height"]
+        thumb_image_resize_width = config["thumb_image_resize_width"]
+        thumb_image_resize_height = config["thumb_image_resize_height"]
 
-        if "scale-width" not in self.options and "scale-height" not in self.options:
+        if "resize-width" not in self.options and "resize-height" not in self.options:
             # No dimensions specified in directive as options. Checking config for defaults.
-            if thumb_image_scale_width is None and thumb_image_scale_height is None:
-                raise self.error('Error in %r directive: "scale-width" option is missing.' % self.name)
+            if thumb_image_resize_width is None and thumb_image_resize_height is None:
+                raise self.error('Error in %r directive: "resize-width" option is missing.' % self.name)
 
         # TODO return
 
@@ -54,7 +54,7 @@ class ThumbImage(ThumbCommon):
 
     def run(self) -> list[Element]:
         """Entrypoint."""
-        self._ThumbCommon__get_scale_size()
+        self._ThumbCommon__get_resize_size()
         return super().run()
 
 
@@ -65,5 +65,5 @@ class ThumbFigure(Figure, ThumbCommon):
 
     def run(self) -> list[Element]:
         """Entrypoint."""
-        self._ThumbCommon__get_scale_size()
+        self._ThumbCommon__get_resize_size()
         return super().run()
