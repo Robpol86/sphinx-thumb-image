305b81acc88425a955754851e147948e870395a6
Robpol86 <robpol86@gmail.com>
2025-12-30 11:21:55 -0600

    Split confoverrides

--- tests/unit_tests/test_target_fullsize.py
+++ tests/unit_tests/test_target_fullsize.py
@@ -12,35 +12,31 @@ from bs4 import element
         (
             {
                 "write_docs": {"index.rst": ".. thumb-image:: _images/tux.png"},
-                "confoverrides": {"thumb_image_resize_width": 100},
             },
             None,
         ),
         (
             {
                 "write_docs": {"index.rst": ".. thumb-image:: _images/tux.png\n  :target-fullsize:"},
-                "confoverrides": {"thumb_image_resize_width": 100},
             },
             "_images/tux.png",
         ),
         (
             {
                 "write_docs": {"index.rst": ".. thumb-image:: _images/tux.png\n  :target: google.com"},
-                "confoverrides": {"thumb_image_resize_width": 100},
             },
             "google.com",
         ),
         (
             {
                 "write_docs": {"index.rst": ".. thumb-image:: _images/tux.png\n  :target: google.com\n  :target-fullsize:"},
-                "confoverrides": {"thumb_image_resize_width": 100},
             },
             "_images/tux.png",
         ),
         (
             {
                 "write_docs": {"index.rst": ".. thumb-image:: _images/tux.png"},
-                "confoverrides": {"thumb_image_resize_width": 100, "thumb_image_target_fullsize": True},
+                "confoverrides": {"thumb_image_target_fullsize": True},
             },
             "_images/tux.png",
         ),
@@ -48,7 +44,7 @@ from bs4 import element
     indirect=["app_params"],
     ids=lambda param: param,  # TODO
 )
-@pytest.mark.sphinx("html", testroot="defaults", srcdir="test_target_original")
-def test_target_original(outdir: Path, img_tags: list[element.Tag], expected: list[int, int]):
+@pytest.mark.sphinx("html", testroot="defaults", confoverrides={"thumb_image_resize_width": 100})
+def test_target_fullsize(outdir: Path, img_tags: list[element.Tag], expected: list[int, int]):
     """TODO."""
     pytest.skip("TODO assert listdir")  # TODO TDD first, then uncomment to confirm :target: works, then implement.
