f7622c7634c926d9521d3162d195678e3b156c18
Robpol86 <robpol86@gmail.com>
2026-01-02 14:34:37 -0600

    Moved to GH issue

--- tests/unit_tests/__init__.py
+++ tests/unit_tests/__init__.py
@@ -2,7 +2,6 @@
 
 TODO::
 - Test filenotfound, same error as ..image
-- Test data: image
 - Test dir/image.* (asset.py)
 - Test file with no extension?
 - Test using relative and absolute paths in directives
