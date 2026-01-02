cd49e0d3591b10549d45a5a1b0267df710834172
Robpol86 <robpol86@gmail.com>
2026-01-02 15:22:17 -0600

    implemented

--- tests/unit_tests/test_error_handling.py
+++ tests/unit_tests/test_error_handling.py
@@ -18,9 +18,30 @@ from sphinx.testing.util import SphinxTestApp
     },
 )
 def test_file_not_found(app: SphinxTestApp, img_tags: list[element.Tag]):
-    """Test with external image."""
+    """Test with image that doesn't exist."""
     # Confirm img src.
     img_src = [t["src"] for t in img_tags]
     assert img_src == ["does_not_exist.jpg"]
     # Confirm warning was emitted.
     assert "WARNING: image file not readable: does_not_exist.jpg" in app.warning.getvalue()
+
+
+@pytest.mark.sphinx(
+    "html",
+    testroot="defaults",
+    warningiserror=False,
+    exception_on_warning=False,
+    confoverrides={
+        "thumb_image_resize_width": 100,
+    },
+    write_docs={
+        "index.rst": ".. thumb-image:: _images",
+    },
+)
+def test_not_a_file(app: SphinxTestApp, img_tags: list[element.Tag]):
+    """Test with image that isn't a file."""
+    # Confirm img src.
+    img_src = [t["src"] for t in img_tags]
+    assert img_src == ["_images/_images"]
+    # Confirm warning was emitted.
+    assert "[Errno 21] Is a directory" in app.warning.getvalue()
