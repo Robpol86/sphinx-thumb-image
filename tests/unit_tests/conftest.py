b35611d6f9f30f07e605750688773c3e6eaa5c0a
Robpol86 <robpol86@gmail.com>
2026-02-18 15:21:53 +0900

    build_return_log

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -35,6 +35,7 @@ def _app_params(app_params, request: pytest.FixtureRequest):
     app_params.kwargs.setdefault("exception_on_warning", True)
     app_params.kwargs.setdefault("warningiserror", True)
     app_params.kwargs.setdefault("freshenv", True)
+    app_params.kwargs.setdefault("verbosity", 2)
     srcdir: Path = app_params.kwargs["srcdir"]
     # Implement copy_files.
     for copy_files in (app_params.kwargs.get("copy_files", []), getattr(request, "param", {}).get("copy_files", [])):
--- tests/unit_tests/test_mtime.py
+++ tests/unit_tests/test_mtime.py
@@ -5,29 +5,22 @@ from datetime import timedelta
 from textwrap import dedent
 
 import pytest
-from bs4 import BeautifulSoup
 from sphinx.testing.util import SphinxTestApp
 
 DELAY = timedelta(seconds=1.5)
 
 
-def write_build_read(app: SphinxTestApp, index_rst_contents: str) -> list[str]:
-    """Overwrite index.rst and build the index.html.
+def build_return_log(app: SphinxTestApp) -> str:
+    """Reset the test environment, Sphinx build, and return the log.
 
     :param app: Sphinx test app.
-    :param index_rst_contents: Write this to the file.
 
-    :return: Parent a.href for each image in index.html.
+    :return: Entire log output for this build.
     """
-    index_rst = app.srcdir / "index.rst"
-    index_html = app.outdir / "index.html"
-
-    index_rst.write_text(index_rst_contents)
+    app.status.truncate(0)
+    app.status.seek(0)
     app.build()
-
-    index_html_contents = index_html.read_text(encoding="utf8")
-    index_html_bs = BeautifulSoup(index_html_contents, "html.parser")
-    return [img.parent.get("href") for img in index_html_bs.find_all("img")]
+    return app.status.getvalue()
 
 
 @pytest.mark.sphinx(
@@ -61,23 +54,21 @@ def test(app: SphinxTestApp):
     )
 
     # Initial build.
-    app.build()
-    log = app.status.getvalue()
-    assert "writing output... [ 33%] control" in log
-    assert "writing output... [ 67%] index" in log
-    assert "writing output... [100%] test" in log
+    log = build_return_log(app)
+    assert "\ndocnames to write: control, index, test\n" in log
 
     # No changes, confirm nothing on rebuild.
     mtimes_before = {k: v.stat().st_mtime for k, v in track_files.items()}
-    app.status.truncate(0)
-    app.build()
-    log = app.status.getvalue()
+    log = build_return_log(app)
     assert "no targets are out of date" in log
     mtimes_after = {k: v.stat().st_mtime for k, v in track_files.items()}
     assert mtimes_before == mtimes_after
 
     # Change control, only control changes.
     time.sleep(DELAY.total_seconds())
+    (app.srcdir / "control.rst").touch(exist_ok=True)
+    log = build_return_log(app)
+    assert "\ndocnames to write: control\n" in log
     pytest.skip("TODO")
 
     # Change thumb, only thumb changes.
