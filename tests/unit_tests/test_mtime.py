e6e3963374bfa4bdda1db4cf6dd1c68fd1399d52
Robpol86 <robpol86@gmail.com>
2026-02-19 15:36:21 +0900

    no control due to bug

--- tests/unit_tests/test_mtime.py
+++ tests/unit_tests/test_mtime.py
@@ -2,29 +2,31 @@
 
 import sys
 import time
-from datetime import timedelta
 from pathlib import Path
 from textwrap import dedent
 
 import pytest
 from sphinx.testing.util import SphinxTestApp
 
-DELAY = timedelta(seconds=1.5)
 
-
-def build_return(app: SphinxTestApp, track_files: dict[str, Path]) -> tuple[str, dict[str, tuple], dict[str, tuple]]:
-    """Reset the test environment, Sphinx build, and return the log and before/after mtimes.
+def rebuild(
+    app: SphinxTestApp, track_files: dict[str, Path], initial: bool = False
+) -> tuple[dict[str, tuple], dict[str, tuple]]:
+    """Reset the test environment, Sphinx build, and return before/after mtimes.
 
     :param app: Sphinx test app.
+    :param track_files: Which files to get mtimes for.
+    :param initial: If this is the first Sphinx build.
 
-    :return: Entire log output for this build and before/after mtimes.
+    :return: Before/after mtimes.
     """
-    mtimes_before = {k: v.stat().st_mtime for k, v in track_files.items()}
-    app.status.truncate(0)
-    app.status.seek(0)
+    if initial:
+        mtimes_before = {}
+    else:
+        mtimes_before = {k: v.stat().st_mtime for k, v in track_files.items()}
     app.build()
     mtimes_after = {k: v.stat().st_mtime for k, v in track_files.items()}
-    return app.status.getvalue(), mtimes_before, mtimes_after
+    return mtimes_before, mtimes_after
 
 
 @pytest.mark.sphinx(
@@ -34,56 +36,49 @@ def build_return(app: SphinxTestApp, track_files: dict[str, Path]) -> tuple[str,
         "thumb_image_resize_width": 100,
     },
     write_docs={
-        "control.rst": dedent("""
-            :orphan:\n
-            .. image:: _images/apple.jpg
-        """),
-        "test.rst": dedent("""
-            :orphan:\n
+        "index.rst": dedent("""
             .. thumb-image:: _images/tux.png
         """),
     },
 )
-@pytest.mark.flaky(reruns=5, condition=sys.platform.startswith("win32"))
+@pytest.mark.flaky(reruns=5, condition=sys.platform.startswith("win32"))  # Windows time precision issues.
 def test_mtime(app: SphinxTestApp):
     """Test cases for the option."""
     track_files = dict(
-        apple_src=app.srcdir / "_images" / "apple.jpg",
         tux_src=app.srcdir / "_images" / "tux.png",
         tux_intermed=app.doctreedir / "_thumbs" / "_images" / "tux.100x118.png",
-        apple_out=app.outdir / "_images" / "apple.jpg",
         tux_out=app.outdir / "_images" / "tux.100x118.png",
-        control_out=app.outdir / "control.html",
         index_out=app.outdir / "index.html",
-        test_out=app.outdir / "test.html",
     )
 
     # Initial build.
-    app.build()
-    log = app.status.getvalue()
-    assert "\ndocnames to write: control, index, test\n" in log
+    rebuild(app, track_files, initial=True)
 
     # No changes, confirm nothing on rebuild.
-    log, mtimes_before, mtimes_after = build_return(app, track_files)
-    assert "\nno targets are out of date.\n" in log
+    mtimes_before, mtimes_after = rebuild(app, track_files)
     assert mtimes_before == mtimes_after
 
-    # Change control, only control changes.
-    time.sleep(DELAY.total_seconds())
-    track_files["apple_src"].touch(exist_ok=True)
-    log, mtimes_before, mtimes_after = build_return(app, track_files)
-    assert "\ndocnames to write: control\n" in log
-    pytest.skip("TODO fix below")
-    apple_out_before = mtimes_before.pop("apple_out")
-    apple_out_after = mtimes_after.pop("apple_out")
-    assert apple_out_before < apple_out_after
-    control_out_before = mtimes_before.pop("control_out")
-    control_out_after = mtimes_after.pop("control_out")
-    assert control_out_before < control_out_after
-    assert mtimes_before == mtimes_after
+    # Change mtime.
+    time.sleep(1.5)  # TODO reduce
+    track_files["tux_src"].touch(exist_ok=True)
+    mtimes_before, mtimes_after = rebuild(app, track_files)
 
-    # Change thumb, only thumb changes.
-    pytest.skip("TODO")
+    tux_intermed_before = mtimes_before.pop("tux_intermed")
+    tux_intermed_after = mtimes_after.pop("tux_intermed")
+    pytest.skip("TODO implement")
+    assert tux_intermed_before < tux_intermed_after
+
+    # TODO uncomment below when resolved: https://github.com/sphinx-doc/sphinx/issues/14312
+    # tux_out_before = mtimes_before.pop("tux_out")
+    # tux_out_after = mtimes_after.pop("tux_out")
+    # assert tux_out_before < tux_out_after
+
+    index_out_before = mtimes_before.pop("index_out")
+    index_out_after = mtimes_after.pop("index_out")
+    assert index_out_before < index_out_after
+
+    assert mtimes_before == mtimes_after
 
     # Confirm no changes again.
-    pytest.skip("TODO")
+    mtimes_before, mtimes_after = rebuild(app, track_files)
+    assert mtimes_before == mtimes_after
